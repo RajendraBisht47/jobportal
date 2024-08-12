f4e8ae83a4752e7572541c09ea949956a28be052
Rajendra Bisht <rajendrasinghbisht7060@gmail.com>
2024-08-12 14:16:34 +0530

    Update Login.jsx

--- Frontend/UI/src/componnects/Login.jsx
+++ Frontend/UI/src/componnects/Login.jsx
@@ -20,7 +20,7 @@ function Login() {
       role: event.target.role.value,
     };
 
-    const response = await fetch("http://localhost:8000/api/v1/user/login", {
+    const response = await fetch("https://jobportal-fmlh.onrender.com/api/v1/user/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(userData),
